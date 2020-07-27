5d0a2c7b73f28803e3a4b6ef4ef7c7d8b80f45ba
Martyn Klassen <lmklassen@gmail.com>
2020-07-27 18:45:37 -0400

    User role-based access control via Keycloak
    - AccessControlID support for authenticated user
    - Obtain accessControlIDs from userinfo token
    - "datacare" role bypasses accessControlID filtering
    - Upstream AE-based access control maintained
    - Don't apply user-based access control when no User Identity Negotiation is present
    - Reject association if user identity is required and cannot be validated using username/password or JWT
    - Optionally use resource access (client) role lookup when performing DICOM user identity negotiation

--- dcm4chee-arc-keycloak/src/main/java/org/dcm4chee/arc/keycloak/AccessTokenRequestor.java
+++ dcm4chee-arc-keycloak/src/main/java/org/dcm4chee/arc/keycloak/AccessTokenRequestor.java
@@ -47,6 +47,7 @@ import org.dcm4che3.net.WebApplication;
 import org.dcm4chee.arc.event.ArchiveServiceEvent;
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.jboss.resteasy.plugins.providers.jackson.ResteasyJackson2Provider;
 import org.keycloak.TokenVerifier;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
@@ -72,7 +73,9 @@ import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
 import java.io.InputStream;
 import java.security.PublicKey;
+import java.util.Collections;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @author Gunter Zeilinger <gunterze@gmail.com>
@@ -90,6 +93,10 @@ public class AccessTokenRequestor {
 
     @Resource
     private ManagedScheduledExecutorService scheduledExecutor;
+
+    public static class KeycloakProvider extends ResteasyJackson2Provider {
+        public KeycloakProvider() {}
+    }
 
     private volatile CachedKeycloak cachedKeycloakClient;
     private volatile CachedPublicKey cachedPublicKey;
@@ -156,12 +163,31 @@ public class AccessTokenRequestor {
         return builder;
     }
 
+    private void parseToken(String accessTokenString, KeycloakClient kc, IdentityConfigurer identityConfigurer) {
+
+        if (identityConfigurer != null) {
+
+            Set<String> accessControlIDs = AccessControl.getTokenAccessControlIDs(accessTokenString, kc);
+
+            identityConfigurer.run(new byte[0],
+                    accessTokenString,
+                    accessControlIDs
+                    );
+        }
+    }
+
     public boolean verifyUsernamePasscode(KeycloakClient kc, String role) throws Exception {
+        return verifyUsernamePasscode(kc, role, null);
+    }
+
+    public boolean verifyUsernamePasscode(KeycloakClient kc, String role, IdentityConfigurer identityConfigurer) throws Exception {
         try (Keycloak keycloak = toKeycloak(kc)) {
             TokenManager tokenManager = keycloak.tokenManager();
-            JWSInput jws = new JWSInput(tokenManager.getAccessToken().getToken());
+            String tokenString = tokenManager.getAccessToken().getToken();
+            JWSInput jws = new JWSInput(tokenString);
             AccessToken token = jws.readJsonContent(AccessToken.class);
-            return role == null || token.getRealmAccess().isUserInRole(role);
+            parseToken(tokenString, kc, identityConfigurer);
+            return AccessControl.isUserInRole(token, role, kc);
         }
     }
 
@@ -175,12 +201,18 @@ public class AccessTokenRequestor {
                 .password(kc.getPassword())
                 .grantType(kc.getKeycloakGrantType().name())
                 .resteasyClient(resteasyClientBuilder(
-                        kc.getKeycloakServerURL(), kc.isTLSAllowAnyHostname(), kc.isTLSDisableTrustManager()).build())
+                        kc.getKeycloakServerURL(), kc.isTLSAllowAnyHostname(), kc.isTLSDisableTrustManager())
+                        .register(KeycloakProvider.class, 1000)
+                        .build()
+                )
                 .build();
     }
 
-
     public boolean verifyJWT(String tokenString, KeycloakClient kc, String role) throws Exception {
+        return verifyJWT(tokenString, kc, role, null);
+    }
+
+    public boolean verifyJWT(String tokenString, KeycloakClient kc, String role, IdentityConfigurer identityConfigurer) throws Exception {
         String serverURL = kc.getKeycloakServerURL();
         String realmName = kc.getKeycloakRealm();
         KeycloakUriBuilder authUrlBuilder = KeycloakUriBuilder.fromUri(serverURL);
@@ -194,7 +226,9 @@ public class AccessTokenRequestor {
         PublicKey publicKey = getPublicKey(kid, jwksUrl, kc);
         tokenVerifier.publicKey(publicKey);
         tokenVerifier.verify();
-        return role == null || tokenVerifier.getToken().getRealmAccess().isUserInRole(role);
+        AccessToken token = tokenVerifier.getToken();
+        parseToken(tokenString, kc, identityConfigurer);
+        return role == null || token.getRealmAccess().isUserInRole(role);
     }
 
     private PublicKey getPublicKey(String kid, String jwksUrl, KeycloakClient kc)
@@ -236,7 +270,11 @@ public class AccessTokenRequestor {
             this.keycloak = keycloak;
         }
     }
-    
+
+    public interface IdentityConfigurer {
+        void run(byte[] response, String accessTokenString, Set<String> accessControlIDs);
+    }
+
     public static class AccessTokenWithExpiration {
         final String token;
         final long expiration;
--- dcm4chee-arc-retrieve/src/main/java/org/dcm4chee/arc/retrieve/impl/RetrieveContextImpl.java
+++ dcm4chee-arc-retrieve/src/main/java/org/dcm4chee/arc/retrieve/impl/RetrieveContextImpl.java
@@ -48,6 +48,8 @@ import org.dcm4che3.net.service.QueryRetrieveLevel2;
 import org.dcm4che3.util.ReverseDNS;
 import org.dcm4che3.util.SafeClose;
 import org.dcm4che3.util.StringUtils;
+import org.dcm4chee.arc.impl.ArchiveAssociationHandler;
+import org.dcm4chee.arc.keycloak.AccessControl;
 import org.dcm4chee.arc.conf.*;
 import org.dcm4chee.arc.entity.Location;
 import org.dcm4chee.arc.entity.Series;
@@ -198,7 +200,12 @@ class RetrieveContextImpl implements RetrieveContext {
 
     @Override
     public String[] getAccessControlIDs() {
-        return arcAE.getAccessControlIDs();
+        return AccessControl.getAccessControlIDs(
+                arcAE.getAccessControlIDs(),
+                this.httpServletRequestInfo,
+                this.requestAssociation,
+                ArchiveAssociationHandler.keycloakClient(arcAE)
+        );
     }
 
     @Override
