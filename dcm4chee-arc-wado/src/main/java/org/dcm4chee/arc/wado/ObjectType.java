0d39ee982450b1d05910138d637fc6b97a6e0406
Igor Solovey <isolovey@robarts.ca>
2021-07-15 14:48:28 -0400

    Add Encapsulated Raw Data storage output
    - Allow access to un-wrapped raw data archive via WADO-URI
    - Add response Content-Disposition header to allow browsers to treat
    the WADO-URI response as a zip archive download

--- dcm4chee-arc-wado/src/main/java/org/dcm4chee/arc/wado/ObjectType.java
+++ dcm4chee-arc-wado/src/main/java/org/dcm4chee/arc/wado/ObjectType.java
@@ -47,6 +47,8 @@ import org.dcm4chee.arc.retrieve.RetrieveContext;
 import org.dcm4chee.arc.store.InstanceLocations;
 
 import javax.ws.rs.core.MediaType;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Stream;
 
@@ -159,8 +161,19 @@ enum ObjectType {
     },
     EncapsulatedMTL(MediaTypes.MODEL_MTL_TYPE, false, false){
         @Override
+        public MediaType[] getRenderedContentTypes() {return null; }
+    },
+    EncapsulatedRaw(MediaTypes.APPLICATION_ZIP_TYPE, false, false){
+        @Override
         public MediaType[] getRenderedContentTypes() {
             return null;
+        }
+
+        @Override
+        public Optional<Map<String, Object>> getResponseHeaders() {
+            Map<String, Object> map = new HashMap<>();
+            map.put("Content-Disposition", "attachment; filename=\"dicom.zip\"");
+            return Optional.of(map);
         }
     },
     EncapsulatedGenozip(MediaTypes.APPLICATION_VND_GENOZIP_TYPE, false, false){
@@ -236,6 +249,9 @@ enum ObjectType {
                 return EncapsulatedOBJ;
             case UID.PrivateDcm4cheEncapsulatedGenozipStorage:
                 return EncapsulatedGenozip;
+            case UID.RawDataStorage:
+                if (EncapsulatedSequenceOutput.isValidInstance(ctx, inst))
+                    return EncapsulatedRaw;
         }
         ArchiveDeviceExtension arcDev = ctx.getArchiveAEExtension().getArchiveDeviceExtension();
         return arcDev.isWadoSupportedSRClass(inst.getSopClassUID())
@@ -245,6 +261,10 @@ enum ObjectType {
 
     public MediaType getDefaultMimeType() {
         return defaultMimeType;
+    }
+
+    public Optional<Map<String, Object>> getResponseHeaders() {
+        return Optional.empty();
     }
 
     public Optional<MediaType> getCompatibleMimeType(MediaType other) {
