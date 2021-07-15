32143e5db5de13fdc2b1292f6d43314f980668e8
Igor Solovey <isolovey@robarts.ca>
2021-07-15 14:48:28 -0400

    Add Encapsulated Raw Data storage output
    - Allow access to un-wrapped raw data archive via WADO-URI
    - Add response Content-Disposition header to allow browsers to treat
    the WADO-URI response as a zip archive download

--- dcm4chee-arc-wado/src/main/java/org/dcm4chee/arc/wado/WadoRS.java
+++ dcm4chee-arc-wado/src/main/java/org/dcm4chee/arc/wado/WadoRS.java
@@ -1340,6 +1340,9 @@ public class WadoRS {
             case EncapsulatedGenozip:
                 entity = new BulkdataOutput(ctx, inst, Tag.EncapsulatedDocument);
                 break;
+            case EncapsulatedRaw:
+                entity = new EncapsulatedSequenceOutput(ctx, inst);
+                break;
             default:
                 throw new AssertionError("Unexpected object type: " + objectType);
         }
