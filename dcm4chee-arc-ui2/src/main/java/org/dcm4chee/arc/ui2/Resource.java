0728ff38f3612b158705e9db809d7550a7fce5a2
Igor Solovey <isolovey@robarts.ca>
2021-07-28 15:12:53 -0400

    Add configurable default Web App Service

--- dcm4chee-arc-ui2/src/main/java/org/dcm4chee/arc/ui2/Resource.java
+++ dcm4chee-arc-ui2/src/main/java/org/dcm4chee/arc/ui2/Resource.java
@@ -88,6 +88,7 @@ public class Resource {
         sb.append("\",\"super-user-role\":\"").append(System.getProperty("super-user-role", "admin"));
         sb.append("\",\"management-http-port\":").append(intSystemProperty("jboss.management.http.port", 9990));
         sb.append(",\"management-https-port\":").append(intSystemProperty("jboss.management.https.port", 9993));
+        sb.append(",\"default-web-app\":\"").append(System.getProperty("default-web-app", "DCM4CHEE")).append("\"");
         sb.append("}");
         return sb.toString();
     }
