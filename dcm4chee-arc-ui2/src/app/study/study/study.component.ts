793f0480bad6bb050b3b6f526c4268b3972f748f
Igor Solovey <isolovey@robarts.ca>
2021-07-28 15:28:32 -0400

    Show ui checkboxes by default

--- dcm4chee-arc-ui2/src/app/study/study/study.component.ts
+++ dcm4chee-arc-ui2/src/app/study/study/study.component.ts
@@ -168,7 +168,7 @@ export class StudyComponent implements OnInit, OnDestroy, AfterContentChecked{
         tableSchema:this.getSchema(),
         config:{
             offset:0,
-            showCheckboxes:false
+            showCheckboxes:true
         }
     };
     // studyDevice:StudyDevice;
