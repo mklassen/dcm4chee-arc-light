2ac8af4ecfd6d2df5ed2ef5983e5c53be91531cd
Igor Solovey <isolovey@robarts.ca>
2021-07-28 15:12:53 -0400

    UI: Add configurable default Web App Service

--- dcm4chee-arc-ui2/src/app/study/study/study.component.ts
+++ dcm4chee-arc-ui2/src/app/study/study/study.component.ts
@@ -10109,6 +10109,21 @@ export class StudyComponent implements OnInit, OnDestroy, AfterContentChecked{
                         webServices: fhirWebApps,
                         allWebServices:fhirWebApps
                     });
+                    if(!this.studyWebService.selectedWebService) {
+                        let defaultWebApp;
+                        this.permissionService.getAppRequest().getDcm4cheeArc().subscribe(res=>{
+                            if(_.hasIn(res, "default-web-app")){
+                                defaultWebApp = _.get(res, "default-web-app");
+                            }
+                        },err=>{
+                            console.log("Error getting defaultWebApp from /dcm4chee-arc/ui2/rs/dcm4chee-arc",err);
+                        });
+                        if (defaultWebApp) {
+                            console.log("defaultWebApp setting is ", defaultWebApp);
+                            this.studyWebService.seletWebAppFromWebAppName(defaultWebApp);
+                            console.log("successfully selected ", defaultWebApp, " as web app");
+                        }
+                    }
                     this.setTemplateToFilter();
                     this.initExporters(2);
                     this.initRjNotes(2);
