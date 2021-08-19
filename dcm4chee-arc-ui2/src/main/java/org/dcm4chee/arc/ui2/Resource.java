458f31db586f53828f7d8935269ccc1ee977a877
Igor Solovey <isolovey@robarts.ca>
2021-08-18 21:29:13 -0400

    Enable customization of ui2 page title

--- dcm4chee-arc-ui2/src/main/java/org/dcm4chee/arc/ui2/Resource.java
+++ dcm4chee-arc-ui2/src/main/java/org/dcm4chee/arc/ui2/Resource.java
@@ -89,6 +89,7 @@ public class Resource {
         sb.append("\",\"management-http-port\":").append(intSystemProperty("jboss.management.http.port", 9990));
         sb.append(",\"management-https-port\":").append(intSystemProperty("jboss.management.https.port", 9993));
         sb.append(",\"default-web-app\":\"").append(System.getProperty("default-web-app", "DCM4CHEE")).append("\"");
+        sb.append(",\"ui2-web-app-title\":\"").append(System.getProperty("ui2-web-app-title", "dcm4chee-arc-ui")).append("\"");
         sb.append("}");
         return sb.toString();
     }
