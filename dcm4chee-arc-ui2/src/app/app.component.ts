b0bcf85727000bf6c16a848a3949e4c11a9bd709
Igor Solovey <isolovey@robarts.ca>
2021-11-09 11:22:29 -0500

    UI: Add Help button

--- dcm4chee-arc-ui2/src/app/app.component.ts
+++ dcm4chee-arc-ui2/src/app/app.component.ts
@@ -46,6 +46,7 @@ export class AppComponent implements OnInit {
     hasAdministrator:boolean = false;
     hasViewRealm:boolean = false;
     authServerUrl;
+    docsUrl = '/docs';
     showMenu = false;
     showScrollButton = false;
     currentServerTime;
@@ -101,7 +102,14 @@ export class AppComponent implements OnInit {
             console.log("Error on /dcm4chee-arc/ui2/rs/dcm4chee-arc",err);
         });*/
 
-
+        this.appRequests.getDcm4cheeArc().subscribe(res=>{
+            if (_.hasIn(res, "documentation-url")){
+                this.docsUrl = _.get(res, "documentation-url");
+            }
+            console.log("docsUrl=",this.docsUrl);
+        },err=>{
+            console.log("Error on /dcm4chee-arc/ui2/rs/dcm4chee-arc",err);
+        });
 
         if(j4care.hasSet(KeycloakService,"keycloakAuth.token")){
             this.mainservice.updateGlobal("notSecure",false);
@@ -339,6 +347,11 @@ export class AppComponent implements OnInit {
             window.open(`//${window.location.hostname}:9990/console`, "_blank");
         }
     }
+
+    navigateToDocs() {
+        window.open(this.docsUrl, "_blank");
+    }
+
     closeFromOutside(){
         if(this.showMenu)
             this.showMenu = false;
--- dcm4chee-arc-ui2/src/main/java/org/dcm4chee/arc/ui2/Resource.java
+++ dcm4chee-arc-ui2/src/main/java/org/dcm4chee/arc/ui2/Resource.java
@@ -90,6 +90,7 @@ public class Resource {
         sb.append(",\"default-web-app\":\"").append(System.getProperty("default-web-app", "DCM4CHEE")).append("\"");
         sb.append(",\"keycloak-use-resource-roles\":\"").append(System.getProperty("keycloak-use-resource-roles", "false")).append("\"");
         sb.append(",\"ui-client-id\":\"").append(System.getProperty("ui-client-id", "dcm4chee-arc-ui")).append("\"");
+        sb.append(",\"documentation-url\":\"").append(System.getProperty("documentation-url", "/docs")).append("\"");
         sb.append(",\"management-https-port\":").append(
                 intSystemProperty("jboss.redirect.management.https.port",
                         intSystemProperty("jboss.management.https.port", 9993)));
