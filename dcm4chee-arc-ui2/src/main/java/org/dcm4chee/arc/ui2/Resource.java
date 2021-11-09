ff575339dfc091832a2c83f9cdbbd479e1247ce4
Igor Solovey <isolovey@robarts.ca>
2021-11-09 11:22:29 -0500

    UI: Add Help button

--- dcm4chee-arc-ui2/src/main/java/org/dcm4chee/arc/ui2/Resource.java
+++ dcm4chee-arc-ui2/src/main/java/org/dcm4chee/arc/ui2/Resource.java
@@ -88,6 +88,7 @@ public class Resource {
         sb.append("\",\"super-user-role\":\"").append(System.getProperty("super-user-role", "root"));
         sb.append("\",\"management-http-port\":").append(intSystemProperty("jboss.management.http.port", 9990));
         sb.append(",\"management-https-port\":").append(intSystemProperty("jboss.management.https.port", 9993));
+        sb.append(",\"documentation-url\":\"").append(System.getProperty("documentation-url", "/docs")).append("\"");
         sb.append(",\"default-web-app\":\"").append(System.getProperty("default-web-app", "DCM4CHEE")).append("\"");
         sb.append(",\"keycloak-use-resource-roles\":\"").append(System.getProperty("keycloak-use-resource-roles", "false")).append("\"");
         sb.append(",\"ui-client-id\":\"").append(System.getProperty("ui-client-id", "dcm4chee-arc-ui")).append("\"");
