db78e4c44d84884488bc6ea23ed340c3b256911c
Igor Solovey <isolovey@robarts.ca>
2022-02-16 15:20:47 -0500

    Optionally configure UI to use resource access roles

--- dcm4chee-arc-ui2/src/main/java/org/dcm4chee/arc/ui2/Resource.java
+++ dcm4chee-arc-ui2/src/main/java/org/dcm4chee/arc/ui2/Resource.java
@@ -87,6 +87,7 @@ public class Resource {
         sb.append("\",\"management-http-port\":").append(
                 intSystemProperty("jboss.management.http.port", 9990));
         sb.append(",\"default-web-app\":\"").append(System.getProperty("default-web-app", "DCM4CHEE")).append("\"");
+        sb.append(",\"ui-client-id\":\"").append(System.getProperty("ui-client-id", "dcm4chee-arc-ui")).append("\"");
         sb.append(",\"management-https-port\":").append(
                 intSystemProperty("jboss.management.https.port", 9993));
         String managementURL = System.getProperty("jboss.ui.management.url");
