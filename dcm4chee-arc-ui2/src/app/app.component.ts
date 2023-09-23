1c197e30617243ba9ac8b812256e84825319938f
Igor Solovey <isolovey@robarts.ca>
2023-09-23 14:20:50 -0400

    Remove server time and HTML title customizations

--- dcm4chee-arc-ui2/src/app/app.component.ts
+++ dcm4chee-arc-ui2/src/app/app.component.ts
@@ -15,7 +15,6 @@ import {PermissionService} from "./helpers/permissions/permission.service";
 import {Observable} from "../../node_modules/rxjs";
 import {HttpClient} from "@angular/common/http";
 import {DcmWebApp} from "./models/dcm-web-app";
-import {Title} from "@angular/platform-browser";
 import {KeycloakService} from "./helpers/keycloak-service/keycloak.service";
 import {Globalvar} from "./constants/globalvar";
 import {KeycloakHttpClient} from "./helpers/keycloak-service/keycloak-http-client.service";
@@ -52,7 +51,6 @@ export class AppComponent implements OnInit {
     showEditAccount = false;
     showScrollButton = false;
     currentServerTime;
-    displayServerTime = false;
     currentClockTime;
     clockInterval;
     j4care = j4care;
@@ -82,7 +80,6 @@ export class AppComponent implements OnInit {
         public config: MatDialogConfig,
         public mainservice: AppService,
         private appRequests: AppRequestsService,
-        private titleService: Title,
         private permissionService:PermissionService,
         private keycloakHttpClient:KeycloakHttpClient,
         private _keycloakService: KeycloakService,
@@ -204,25 +201,22 @@ export class AppComponent implements OnInit {
             return `${this.getFullYear()}${j4care.getSingleDateTimeValueFromInt(this.getMonth()+1)}${j4care.getSingleDateTimeValueFromInt(this.getDate())}${j4care.getSingleDateTimeValueFromInt(this.getHours())}${j4care.getSingleDateTimeValueFromInt(this.getMinutes())}${j4care.getSingleDateTimeValueFromInt(this.getSeconds())}`;
         };
         this.initGetDevicename(2);
-        this.setTitle();
 /*        this.setServerTime(()=>{
         });*/
 
-        if ( this.displayServerTime ) {
-            document.addEventListener("visibilitychange", () => {
-                if(document.visibilityState === "visible"){
-                    this.startTime();
-                }else{
-                    if(worker){
-                        worker.postMessage({
-                            serverTime:this.currentServerTime,
-                            idle:document.hidden
-                        });
-                    }
+        document.addEventListener("visibilitychange", () => {
+            if(document.visibilityState === "visible"){
+                this.startTime();
+            }else{
+                if(worker){
+                    worker.postMessage({
+                        serverTime:this.currentServerTime,
+                        idle:document.hidden
+                    });
                 }
-            });
+            }
+        });
         }
-    }
     startTime(){
         if (typeof Worker !== 'undefined') {
             worker.onmessage = ({data}) => {
@@ -479,11 +473,11 @@ export class AppComponent implements OnInit {
                 (res) => {
                     // $this.mainservice["deviceName"] = res.dicomDeviceName;
                     this.initGetPDQServices();
+                    this.startTime();
                     this.dcm4cheeArch = res;
                     $this.mainservice["xRoad"] = res.xRoad || false;
                     $this.mainservice["management-http-port"] = res["management-http-port"] || 9990;
                     $this.mainservice["management-https-port"] = res["management-https-port"] || 9993;
-                    this.displayServerTime = (res["display-server-time"] || "true").toLowerCase() === "true";
                     this.docsUrl = _.get(res, "documentation-url");
                     this.appRequests.getDeviceInfo(res.dicomDeviceName)
                         .subscribe(
@@ -512,14 +506,6 @@ export class AppComponent implements OnInit {
         this.appRequests.getPDQServices().subscribe(pdqs=>{
             this.mainservice.updateGlobal("PDQs",pdqs);
         })
-    }
-
-    setTitle() {
-        this.appRequests.getDeviceName()
-            .subscribe(
-                (res) => {
-                    this.titleService.setTitle(res['ui2-web-app-title']);
-                })
     }
 
 }
