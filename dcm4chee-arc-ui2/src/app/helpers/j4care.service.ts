1294887e628224f750ae1f0e1ee09fc8e2a70983
Martyn Klassen <lmklassen@gmail.com>
2025-04-09 15:29:05 -0400

    Fix the automated tests

--- dcm4chee-arc-ui2/src/app/helpers/j4care.service.ts
+++ dcm4chee-arc-ui2/src/app/helpers/j4care.service.ts
@@ -1390,6 +1390,9 @@ export class j4care {
     */
     static setZeroPrefix(str){
         try{
+            if (str == null)
+                return str;
+
             if(typeof str === 'number'){
                 str = str.toString();
             }
@@ -1605,6 +1608,9 @@ export class j4care {
     * */
     static join(array:string[],joinString:string, lastJoinString?:string){
         try{
+            if (array == null)
+                return "";
+
             if(array.length > 1){
                 if(lastJoinString){
                     return `${array.slice(0,-1).join(joinString)}${lastJoinString}${array.slice(-1)}`;
